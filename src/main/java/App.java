21ca593ba07f0228ebde235a1574542cafa6598b
Amaeretran <107470981+Amaeretran@users.noreply.github.com>
2022-08-21 21:07:25 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
+        System.out.println(bean.getMessage());
+    }
+}
